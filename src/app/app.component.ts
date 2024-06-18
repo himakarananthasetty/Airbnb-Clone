bbb0927f2c2d9bb0f3b85c64344cd68fcb6230f3
Himakar Ananthasetty <himakarananthasetty@gmail.com>
2024-06-17 20:06:25 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Button} from "primeng/button";
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Button, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'airbnb-clone-frontend';
+  faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit() {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+    }
+
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,7 @@
+import {IconDefinition} from "@fortawesome/angular-fontawesome";
+import {faAirbnb} from "@fortawesome/free-brands-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faAirbnb
+]
+
